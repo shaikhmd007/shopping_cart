583c54e6b0c1e67983124142c770bcc9757cac0a
shaikh_md.007 <shaikhmohammad520@gmail.com>
2022-06-19 19:26:05 +0530

    first commit

---
+++ precache-manifest.0e204766210dbecc862b2ba2f232a601.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de4e767583ffeea2ab73655107e4ac95",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd598ae03e34ee870cc7",
+    "url": "/static/css/main.4c3f58cd.chunk.css"
+  },
+  {
+    "revision": "dd9d7c201df0317fcddb",
+    "url": "/static/js/2.22bb3313.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.22bb3313.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd598ae03e34ee870cc7",
+    "url": "/static/js/main.f9a9313e.chunk.js"
+  },
+  {
+    "revision": "a8a825e3b25e51a50344",
+    "url": "/static/js/runtime-main.a35414a8.js"
+  }
+]);
